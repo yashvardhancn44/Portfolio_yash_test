99a7ee821935b84a168dc5bd19537b405d4bdf22
yashvardhancn44 <zbti.dev@gmail.com>
2024-09-24 18:27:40 +0530

    added a new Project

--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,7 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import insightdashboard from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -76,11 +76,10 @@ export const projectsData = [
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    title: "Insight Dashboard",
+    description: "A dashboard which shows data in may graphs",
+    tags: ["React", "ReCharts", "Redux"],
+    imageUrl: insightdashboard,
   },
 ] as const;
 
