8402e30a1fdae8afe2010981bdb605a93e08fd1c
yashvardhancn44 <zbti.dev@gmail.com>
2024-10-05 20:39:01 +0530

    Added few more projects

--- lib/data.ts
+++ lib/data.ts
@@ -2,13 +2,13 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import insightdashboardImg from "@/public/insightdashboard.png";
 import insightDashImg from "@/public/insightDash.png";
 import portfolioImg from "@/public/portfolio.png";
 import notesappImg from "@/public/notesApp.png";
 import yashcartImg from "@/public/yashcart.png";
+import kakfaImg from "@/public/KafkaApp.png";
+import recipeImg from "@/public/recipeApp.png";
+import jobsImg from "@/public/reactjobshome.png";
 
 export const links = [
   {
@@ -42,7 +42,7 @@ export const experiencesData = [
     title: "Bachelor of Engineering",
     location: "Siddaganga Institute of Technology, Tumkuru",
     description:
-      "Graduated in Electronics and Communication. Worked on projects related to Robotics and Artificial intelligence (M.L. and Image processing). Published 3 research papers in IEEE and Springer",
+      "Graduated in Electronics and Communication, worked on challenging research projects in bipedal robots and hand gesture recognition systems. Published 3 research papers in IEEE and Springer. Represented the college in basketball, served as design lead, and editorial member for the departmental magazine.",
     icon: React.createElement(LuGraduationCap),
     date: "2017-2021",
   },
@@ -50,7 +50,7 @@ export const experiencesData = [
     title: "Vice Chairpeson of IEEE SIT student branch",
     location: "Siddaganga Institute of Technology, Tumkuru",
     description:
-      "Led a cross cultural team of 117 people in organizing National level events, Hackathons, Competetions. Led the team during Covid, successfully transitioned all events to online mode and achieved success.",
+      "Strategically led a cross-cultural team of 117 people in organizing national-level events during COVID-19, engaging over 1,000 participants. Managed 10+ workshops, hackathons, and competitions, while leading the design, marketing, and sponsorship teams for 3 consecutive years.",
     icon: React.createElement(LuGraduationCap),
     date: "2020-2021",
   },
@@ -58,7 +58,7 @@ export const experiencesData = [
     title: "Software Engineer",
     location: "Cisco Systems, Bengaluru",
     description:
-      "Part of the financial reporting team of CISCO, developing big data and web applications. Developed a Kafka consumer for data retrieval using Python, Kafka, and PostgreSQL, optimizing the transfer of 1 million data records in 130 seconds thereby saving time and cost for the team. Developed a dashboard web application for POC using ReactJS.",
+      "Part of the financial reporting team of CISCO, developing big data and web applications. Developed a Kafka consumer for data retrieval using Python, Kafka, and PostgreSQL, optimizing the transfer of 1 million data records in 130 seconds thereby saving time and cost for the team. Developed a dashboard web application for POC using ReactJS. Engaged in collaborative, cross-cultural Agile development, and played a key role in team meetings.",
     icon: React.createElement(CgWorkAlt),
     date: "2021",
   },
@@ -71,39 +71,12 @@ export const experiencesData = [
     date: "2022 - present",
   },
 ] as const;
-/*
-export const experiencesData = [
-  {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
-] as const;
- */
 
 export const projectsData = [
   {
     title: "Yashcart",
-    description: "An Ecommerce Website.",
+    description:
+      "An eCommerce app with product browsing, JWT authentication, checkout, and admin management.",
     tags: [
       "React",
       "Express.js",
@@ -115,55 +88,67 @@ export const projectsData = [
     gitlink: "https://github.com/yashvardhancn44/Ecommerce-website",
   },
   {
+    title: "User Interaction Analytics",
+    description:
+      " Real-time analytics system capturing interactions, streaming to Kafka, generating dashboard.",
+    tags: [
+      "React",
+      "KafkaJS",
+      "Express.js",
+      "Recharts",
+      "MongoDB",
+      "Websockets",
+    ],
+    imageUrl: kakfaImg,
+    gitlink: "https://github.com/yashvardhancn44/User-Interaction-Analytics",
+  },
+  {
     title: "Portfolio",
-    description: "A responsive and animative Portfolio website.",
-    tags: ["React", "Next.js", "TypeScript", "Next.js", "Tailwind"],
+    description:
+      "Dynamic portfolio app showcasing experience, projects, skills, with animations and responsiveness.",
+    tags: ["React", "Next.js", "TypeScript", "Tailwind CSS", "Framer Motion"],
     imageUrl: portfolioImg,
     gitlink: "https://github.com/yashvardhancn44/Portfolio_yash_test",
   },
   {
-    title: "Insight Dashboard",
-    description: "A dashboard which shows data in may graphs",
-    tags: ["React", "ReCharts", "Redux"],
-    imageUrl: insightDashImg,
-    gitlink: "https://github.com/yashvardhancn44",
+    title: "Recipe App",
+    description:
+      "Recipe search app using Spoonacular API, saving favorites in local storage",
+    tags: ["React", "Express.js", "PostgreSQL", "Prisma", "Typescript"],
+    imageUrl: recipeImg,
+    gitlink: "https://github.com/yashvardhancn44/Recipe-App-Frontend",
   },
   {
     title: "Notes App",
-    description: "A Full stack Notes app",
+    description:
+      "Full-stack Notes app with add, update, delete, and local storage persistence.",
     tags: ["React", "Express.js", "Prisma", "PostgresSQL"],
     imageUrl: notesappImg,
     gitlink: "https://github.com/yashvardhancn44/Notes-App-Full-Stack-Frontend",
   },
-] as const;
-
-/*
-export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Job Search Website",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-    gitlink: "https://github.com/yashvardhancn44",
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-    gitlink: "https://github.com/yashvardhancn44",
+      "Frontend job website enabling employers to add jobs and developers to search.",
+    tags: [
+      "React",
+      "Tailwind CSS",
+      "JSON-server",
+      "React-router-dom",
+      "React-toastify",
+    ],
+    imageUrl: jobsImg,
+    gitlink: "https://github.com/yashvardhancn44/JobsListing-Frontend",
   },
   {
     title: "Insight Dashboard",
-    description: "A dashboard which shows data in may graphs",
+    description:
+      "Dashboard app visualizing data with charts using the Recharts library",
     tags: ["React", "ReCharts", "Redux"],
-    imageUrl: insightdashboardImg,
-    gitlink: "https://github.com/yashvardhancn44",
+    imageUrl: insightDashImg,
+    gitlink: "https://github.com/yashvardhancn44/Insight-Dashboard-frontend",
   },
 ] as const;
- */
 
 export const skillsData = [
   "HTML",
@@ -173,6 +158,7 @@ export const skillsData = [
   "Python",
   "React.js",
   "Express.js",
+  "Kafka.js",
   "Next.js",
   "Node.js",
   "Redux-Toolkit",
